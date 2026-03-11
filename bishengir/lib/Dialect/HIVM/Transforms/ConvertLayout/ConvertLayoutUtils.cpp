b9cfc9a455f270142eb85cde943aeb97c275567a
Hocky Yudhiono <htxdy@outlook.com>
2026-03-11 18:36:42 +0800

    [AscendNPU IR] Add PropagateConvertLayout patterns

--- bishengir/lib/Dialect/HIVM/Transforms/ConvertLayout/ConvertLayoutUtils.cpp
+++ bishengir/lib/Dialect/HIVM/Transforms/ConvertLayout/ConvertLayoutUtils.cpp
@@ -68,10 +68,8 @@ bool isPropagatingDown(ConvertLayoutOp op) {
 }
 
 bool isLayoutAgnosticOp(Operation *op) {
-  // TODO: When propagating is fixed, can remove this following line
   if (!op)
     return false;
-  return isa<hivm::VCastOp>(op);
   if (auto vbrcOp = dyn_cast<VBrcOp>(op)) {
     return isScalarLike(vbrcOp.getSrc().getType());
   }
