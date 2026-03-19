44543fb93e89e11083fd1c8592700ca1dd9fc69d
weilinquan <weilinquan1@huawei.com>
2026-03-19 06:39:04 +0000

    [Huawei][AscendNPU IR] Add disable-fma option
    Offering: BiSheng

--- bishengir/include/bishengir/Tools/bishengir-compile/Config.h
+++ bishengir/include/bishengir/Tools/bishengir-compile/Config.h
@@ -97,6 +97,12 @@ public:
   }
   bool shouldDisableFFTS() const { return disableFFTSFlag; }
 
+  BiShengIRCompileMainConfig &setDisableFMA(bool flag) {
+    disableFMAFlag = flag;
+    return *this;
+  }
+  bool shouldDisableFMA() const { return disableFMAFlag; }
+
   BiShengIRCompileMainConfig &compileTriton(bool compile) {
     enableTritonKernelCompileFlag = compile;
     return *this;
@@ -735,6 +741,9 @@ protected:
 
   /// Force enable FFTS for cube kernels
   bool disableFFTSFlag{false};
+
+  /// Force enable FMA for simt kernels
+  bool disableFMAFlag{false};
 
   /// Enable Triton kernel compile.
   bool enableTritonKernelCompileFlag{false};
