0e0fe0123d7f69f654c29e2706b2d51492a35e16
wzy_00889928 <1322947566@qq.com>
2026-06-01 20:18:25 +0800

    [Huawei][AscendNPU IR] feat: cv-default-disable-flatten.

--- bishengir/lib/Tools/bishengir-compile/PassPipeline.cpp
+++ bishengir/lib/Tools/bishengir-compile/PassPipeline.cpp
@@ -45,6 +45,7 @@
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Target/LLVMIR/ModuleTranslation.h"
 #include "mlir/Transforms/Passes.h"
+#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 
 #include <set>
@@ -268,9 +269,20 @@ static void buildDelayedHFusionRegBaseVectorizePipeline(
   if (config.getDisableHfusionVectorize()) {
     return;
   }
+  // inferMixedCV populates enableMixedCV before this delayed HFusion pipeline
+  // is built; adjust only the local HFusion options consumed by flatten.
+  BiShengIRCompileMainConfig hfusionConfig = config;
+  auto &registeredOptions = llvm::cl::getRegisteredOptions();
+  auto enableFlattenOpt = registeredOptions.find("enable-flatten");
+  bool hasExplicitEnableFlatten =
+      enableFlattenOpt != registeredOptions.end() &&
+      enableFlattenOpt->second->getNumOccurrences() != 0;
+  if (hfusionConfig.shouldEnableMixedCV() && !hasExplicitEnableFlatten) {
+    hfusionConfig.setEnableFlatten(false);
+  }
 
   hfusion::HFusionPipelineOptions hfusionPipelineOptions;
-  setupHFusionPipelineOptions(hfusionPipelineOptions, config);
+  setupHFusionPipelineOptions(hfusionPipelineOptions, hfusionConfig);
   ExecutionEngineHIVMToUpstreamConversionOptions upstreamOptions;
   upstreamOptions.convertToNamedOp = 
       hacc::utils::isRegBasedArch(config.getTarget());
