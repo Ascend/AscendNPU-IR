40202fd76218eb8c1ee8e59987a0df462ac94eec
kang-ingu <kang.ingu@huawei.com>
2026-07-28 12:03:59 +0800

    [AscendNPU IR]: Temporarily disable tiling reduction dim when the size is 1xN

--- bishengir/lib/Dialect/HIVM/Analysis/DimensionAnalyzer.cpp
+++ bishengir/lib/Dialect/HIVM/Analysis/DimensionAnalyzer.cpp
@@ -355,8 +355,12 @@ void DimensionAnalyzer::computeTilingDimImpl(
       Dimension dim(src, i);
       if (isParallelDim(dim)) {
         if (ShapedType::isDynamic(shape[i]) || shape[i] == 1) {
-          if (!checkTileableMaskedStore(op, i))
+          if (!checkTileableMaskedStore(op, i)) {
+            // temporarily disable tiling for 1xN VReduce
+            if constexpr (std::is_same_v<StoreOpTy, hivm::VReduceOp>)
+              break;
             continue;
+          }
         }
         if constexpr (std::is_same_v<StoreOpTy, hivm::VReduceOp>) {
           if (shape[i] <= 4)
