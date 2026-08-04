75ebe9d2e847dba0a742b430d9adf99d8fa14c9d
Roman Rusyaev <rusyaev.roman.mikhailovich@huawei.com>
2026-08-04 21:45:49 +0000

    [AscendNPU-IR] enable flatten by default

--- bishengir/lib/Dialect/HIVMAVE/Pipelines/HIVMAVEPipelines.cpp
+++ bishengir/lib/Dialect/HIVMAVE/Pipelines/HIVMAVEPipelines.cpp
@@ -51,14 +51,12 @@ static void hivmAVEOptimizationPipeline(
     OptimizeReductionLoopHIVMAVEOptions optimizeReductionLoopOptions;
     optimizeReductionLoopOptions.maxSplit =
         hivmAVEPipelineOptions.maxReductionSplitNum;
-    // Vsstb packing depends on adjacent store order; reduction splitting may
-    // pair non-adjacent IVs (e.g. i and i + half) and hide that pattern.
-    pm.nest<func::FuncOp>().addPass(hivmave::createProcessVsstbPass());
     pm.nest<func::FuncOp>().addPass(
         hivmave::createOptimizeReductionLoopHIVMAVEPass(
             optimizeReductionLoopOptions));
     if (hivmAVEPipelineOptions.enableAveLoopOptimize)
       pm.nest<func::FuncOp>().addPass(hivmave::createAveLoopOptimizePass());
+    pm.nest<func::FuncOp>().addPass(hivmave::createProcessVsstbPass());
     pm.nest<func::FuncOp>().addPass(hivmave::createLegalizeOptHIVMAVEPass());
     pm.nest<func::FuncOp>().addPass(
         hivmave::createReplaceWithVectorScalarPass());
