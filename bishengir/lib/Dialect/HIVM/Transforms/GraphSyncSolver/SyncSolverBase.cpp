28ceefa9bb29fee31c8f5f0a0d34aec3758dc1bd
Frank (Fang) Gao <fang.gao1@huawei.com>
2026-08-24 18:12:17 +0000

    [CVPipeline] feat: support loop-carried-dependence

--- bishengir/lib/Dialect/HIVM/Transforms/GraphSyncSolver/SyncSolverBase.cpp
+++ bishengir/lib/Dialect/HIVM/Transforms/GraphSyncSolver/SyncSolverBase.cpp
@@ -1209,7 +1209,7 @@ SyncSolverBase::checkCVPreloadingEventIdInfo(Occurrence *occ1, Occurrence *occ2,
                                  : 1;
 
     if (options.isCrossCoreMode()) {
-      assert(preloadDiff % 2 == 1);
+      assert(preloadDiff % 2 == 1 || preloadDiff == 0);
       if (multibufferNum >= eventIdNum) {
         EventIdInfo eventIdInfo(multibufferNum);
         eventIdInfo.cvPreloadingInfo =
@@ -1225,7 +1225,8 @@ SyncSolverBase::checkCVPreloadingEventIdInfo(Occurrence *occ1, Occurrence *occ2,
             CVPreloadingInfo(parentCVPipeliningLoop1, parentScope1,
                              parentScope2, preloadOffset1, preloadOffset2);
         return eventIdInfo;
-      } else if (multibufferNum == 1) {
+      }
+      if (multibufferNum == 1) {
         // instead of inserting outside of the scopes, use the unlikely trick
         EventIdInfo eventIdInfo(1);
         eventIdInfo.cvPreloadingInfo =
