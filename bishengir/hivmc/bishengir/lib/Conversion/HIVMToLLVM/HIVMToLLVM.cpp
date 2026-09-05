c1ab4926d159b63b5b778e2d6924a1097c088a8b
chenwuyang <chenwuyang1@huawei.com>
2026-09-05 09:22:15 +0800

    [Huawei][HIVMToLLVM] fix: ssbuf loadOp defaut to volatile since it was removed in previous passes
    Signed-off-by: chenwuyang <chenwuyang1@huawei.com>

--- bishengir/hivmc/bishengir/lib/Conversion/HIVMToLLVM/HIVMToLLVM.cpp
+++ bishengir/hivmc/bishengir/lib/Conversion/HIVMToLLVM/HIVMToLLVM.cpp
@@ -163,21 +163,10 @@ struct ConvertHIVMToLLVM
     // mark ssbuf-related load/store ops as volatile to prevent ccec from
     // hoisting
     auto loadOpWalkFn = [](LLVM::LoadOp loadOp) {
-      if (!isLLVMLoadStoreOnSsbuf(loadOp)) {
-        return WalkResult::advance();
+      if (isLLVMLoadStoreOnSsbuf(loadOp)) {
+        // TODO: think of a better way of handling ssbuf loads.
+        loadOp.setVolatile_(true);
       };
-      auto markOpOpt = utils::getAnnotateOpWithAttr(loadOp, kMemrefExtVolatile);
-      if (!markOpOpt.has_value()) {
-        return WalkResult::advance();
-      }
-      loadOp.setVolatile_(true);
-      auto markOp = markOpOpt.value();
-      markOp->removeAttr(kMemrefExtVolatile);
-      if (llvm::all_of(markOp->getAttrs(), [](NamedAttribute attr) {
-            return attr.getName() == "effect";
-          })) {
-        markOp->erase();
-      }
       return WalkResult::advance();
     };
 
